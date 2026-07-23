42c99a4ca321e935405a433db6e434fc5ff8a641
david malca garcia <davidmalcagarcia090205@gmail.com>
2026-07-23 18:40:00 -0500

    Aquí escribes tu mensaje de lo que cambiaste

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('' , views.login_usuario),
     path('enlace_panel_control_almacen/' , views.enlace_panel_control_almacen),
     path('enlace_tabla_horario/' , views.enlace_tabla_horario),
-    path('enlace_registrar_horario/' , views.enlace_registrar_horario)
+    path('enlace_registrar_horario/' , views.enlace_registrar_horario),
+    path('logica_registrar_horario/' , views.logica_registrar_horario)
 ]
--- app/views.py
+++ app/views.py
@@ -17,3 +17,20 @@ def enlace_tabla_horario(request):
 def enlace_registrar_horario(request): 
     return render(request , 'registrar_horario.html')
 
+def logica_registrar_horario(request): 
+    Id_tabla = request.POST['textId']
+    Nombre_tabla = request.POST['textNombre']
+    Fecha_inicio = request.POST['textFechaRegistro']
+    Hora_inicio = request.POST['textHoraInicio']
+    Hora_final = request.POST['textHoraSalida']
+    Comentario = request.POST['textComentario']
+
+    horario = Tabla_Horario_Tecnico.objects.create(
+        Id_tabla_horario = Id_tabla,
+        Nombre_tabla_horario = Nombre_tabla,
+        Fecha_inicio_horario = Fecha_inicio,
+        Hora_inicio_horario = Hora_inicio,
+        Hora_final_horario = Hora_final,
+        Comentario_horario = Comentario,
+    )
+    return redirect("/enlace_tabla_horario/")
