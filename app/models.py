05d1136ede19c99b96a9751614f7555e34d59652
david malca garcia <davidmalcagarcia090205@gmail.com>
2026-07-22 21:12:31 -0500

    commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
 # Create your models here.
+class  Tabla_Horario(models.Model):
+    Id_tabla_horario = models.CharField(max_length=250)
+    Nombre_tabla_horario = models.TextField(max_length=250)
+    Hora_inicio_horario = models.TimeField()
+    Hora_final_horario = models.TimeField()
+    Comentario_horario = models.TextField(max_length=250)
+
+class  Tabla_Hora_Extra(models.Model): 
+    Id_tabla_hora_extra = models.CharField(max_length=250)
+    Nombre_hora_extra = models.CharField(max_length=250)
+    Fecha_hora_extra = models.DateField()
+    Hora_inicio_extra = models.TimeField()
+    Hora_final_extra = models.TimeField()
+    Cantidad_hora_extra = models.CharField(max_length=250)
+    Comentario_hora_extra = models.CharField(max_length=250)
+
+class Tabla_Recepcion_Material(models.Model): 
+    Id_recepcion_material = models.CharField(max_length=250)
+    Fecha_recepcion_material = models.DateField()
+    Nombre_empresa_recepcion_material = models.CharField(max_length=250)
+    Nombre_material_recepcion_material = models.CharField(max_length=250)
+    Numero_factura_recepcion_material = models.CharField(max_length=250)
+    Codigo_recepcion_material = models.CharField(max_length=250)
+    Cantidad_recepcion_material = models.CharField(max_length=250)
+
+class Tabla_Salida_Material(models.Model):
+    Id_tabla_salida_material = models.CharField(max_length=250)
+    Fecha_salida_material = models.DateField()
+    Nombre_salida_material = models.CharField(max_length=250)
+    Descripcion_salida_material = models.CharField(max_length=250)
+    Padron_salida_material = models.CharField(max_length=250)
+    Cantidad_salida_material = models.CharField(max_length=250)
+
+class Tabla_Cambio_Material_Vehicular(models.Model):
+    Id_cambio_cambio_material_vahicular = models.CharField(max_length=250)
+    Fecha_registro_cambio_material_vehicular = models.DateField()
+    Fecha_emision_cambio_material_vehicular = models.DateField()
+    Fecha_vencimiento_cambio_material_vehicular = models.DateField()
+    Padron_cambio_material_vehicular = models.CharField(max_length=250)
+    Placa_cambio_material_vehicular = models.CharField(max_length=250)
+    Cantidad_cambio_material_vehicular = models.CharField(max_length=250)
+    Comentario_cambio_material_vehicular = models.CharField(max_length=250)
+
+class Tabla_Inventario(models.Model):
+    pass 
+
+class Tabla_Tecnico_Mecanico(models.Model):
+    pass 
+
+class Tabla_Trabajo_Mecanico(models.Model):
+    pass
